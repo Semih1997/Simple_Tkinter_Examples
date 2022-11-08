d2fef8007098dbd10632e4fd361447fb20e99356
Semih1997 <emiroglusemih97@gmail.com>
2022-11-08 10:23:44 +0300

    Create Calculator_interface.py
    First touch to calculator

---
+++ Calculator_interface.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+calculator_screen = Tk()
+
+calculator_screen.title("CALCULATOR")
+calculator_screen.geometry("300x500")
+
+calculator_screen.resizable(False,False)
+calculator_screen.mainloop()
