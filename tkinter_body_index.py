f82b06ac9d5efc284a8cc42893470c0099887d9c
Semih1997 <emiroglusemih97@gmail.com>
2022-11-11 05:57:27 +0300

    Add info

--- tkinter_body_index.py
+++ tkinter_body_index.py
@@ -6,7 +6,7 @@ labels_width = 200
 body_index_screen = Tk()
 
 def body_index_calculate():
-    height = height_entry.get()
+    height = height_entry.get()  # It is allows us to get the number in the entry
     height = float(height) / 100
     kilogram = kilogram_entry.get()
     kilogram = float(kilogram)
@@ -19,15 +19,13 @@ body_index_screen.title("Body Index Calculator")
 
 kilogram_label = Label(body_index_screen,text="Kilogram =").place(x=10,y=10)
 kilogram_entry = Entry(body_index_screen,width=20)
-kilogram_entry.place(x=150,y=10)
+kilogram_entry.place(x=150,y=10)  # If you write the .place function to previous line .get is not working.
 
 height_label = Label(body_index_screen,text="Height(santimeter) =").place(x=10,y=40)
 height_entry = Entry(body_index_screen,width=20)
 height_entry.place(x=150,y=40)
 
 calculate_button = Button(body_index_screen,text="CALCULATE",command=lambda :body_index_calculate()).place(x=100,y=85)
-# calculated_label = Label(body_index_screen,text="").place(x=150,y=125)
-# calculated_text = Label(body_index_screen,text="").place(x=30,y=125)
 
 body_index_screen.resizable(False,False)
 body_index_screen.mainloop()
